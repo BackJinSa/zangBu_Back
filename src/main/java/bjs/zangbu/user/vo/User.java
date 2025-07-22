fcfd0d1841a3b11efac30dc7a2f2db1cf1978c98
M6F\student <sojsnake@gmail.com>
2025-07-22 14:09:29 +0900

    test

--- src/main/java/bjs/zangbu/user/vo/User.java
+++ src/main/java/bjs/zangbu/user/vo/User.java
@@ -1,4 +1,39 @@
 package bjs.zangbu.user.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class User {
+    //유저 식별 id
+    private UUID userId;
+
+    //아이디(이메일)
+    private String email;
+
+    //비밀번호
+    private String password;
+
+    //전화번호
+    private String phone;
+
+    //닉네임
+    private String nickname;
+
+    //주민번호
+    private String identity;
+
+    //역할(관리자, 사용자)
+    private UserEnum role;
+
+    //생년월일 6자리
+    private char birth;
+
+    //사용자 이름
+    private String userName;
 }
---
+++ src/main/java/bjs/zangbu/user/vo/UserEnum.java
@@ -0,0 +1,6 @@
+package bjs.zangbu.user.vo;
+
+public enum UserEnum {
+    ADMINISTER, //관리자
+    USER //사용자
+}
