138585cd6385eaecd8fb89962654ed6d9b5084c9
zefa42 <youngoh07015@gmail.com>
2025-07-22 15:59:09 +0900

    feat: map 기능의 controller 구현 및 예외 처리

--- src/main/java/bjs/zangbu/map/controller/MapController.java
+++ src/main/java/bjs/zangbu/map/controller/MapController.java
@@ -1,4 +1,34 @@
 package bjs.zangbu.map.controller;
 
+import bjs.zangbu.map.dto.request.MapRequest;
+import bjs.zangbu.map.dto.response.MapResponse;
+import bjs.zangbu.map.service.MapService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/map")
+@RequiredArgsConstructor
 public class MapController {
+    private final MapService mapService;
+
+    @PostMapping("/list")
+    public ResponseEntity<?> list(@RequestBody List<MapRequest> body) {
+        try {
+            List<MapResponse> result = mapService.locate(body);
+            return ResponseEntity.ok(result);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest()
+                    .body("매물을 불러오는데 실패했습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500)
+                    .body("서버에서 매물을 불러오는데 실패횄습니다.");
+        }
+    }
 }
