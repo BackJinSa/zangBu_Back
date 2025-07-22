92d25e5ca66085febdc2aeeb60302273bd669646
zefa42 <youngoh07015@gmail.com>
2025-07-22 14:37:44 +0900

    feat: map 기능 dto, vo 구현

--- src/main/java/bjs/zangbu/map/dto/request/MapRequest.java
+++ src/main/java/bjs/zangbu/map/dto/request/MapRequest.java
@@ -1,4 +1,11 @@
 package bjs.zangbu.map.dto.request;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class MapRequest {
+    private String address;
+    private String buildingName;
 }
--- src/main/java/bjs/zangbu/map/vo/MapLocation.java
+++ src/main/java/bjs/zangbu/map/vo/MapLocation.java
@@ -1,4 +1,15 @@
 package bjs.zangbu.map.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MapLocation {
+    private String address;
+    private String latitude;
+    private String longitude;
+    private String buildingName;
 }
