3716a2ef990f102bbacf7ecb4074303301901fcb
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-07-22 23:56:46 +0900

    feat:매물 상세보기 기능 개발

--- src/main/java/bjs/zangbu/building/controller/BuildingController.java
+++ src/main/java/bjs/zangbu/building/controller/BuildingController.java
@@ -13,13 +13,16 @@ import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequiredArgsConstructor
+// building 엔드포인트 시작을 위해서 RequestMapping을 해줌.
 @RequestMapping("/building")
 public class BuildingController {
-
+    // BuildingService로 요청을 보내기 위해서 private final BuildingService buildingSerivce를 선언하고 RequiredArgsConsturctor 어노테이션을 붙혔다.
     private final BuildingService buildingService;
     @PostMapping("")
     public ResponseEntity<ViewDetailResponse> viewDetail(ViewDetailRequest request)
+    // 이거는 Codef API를 쓸라면 무조건 에러 처리를 해야하는데 이걸 전역에러 처리하기엔 Codef에서만 사용하기때문에 이렇게 처리했다.
             throws UnsupportedEncodingException, JsonProcessingException, InterruptedException {
+        // service에 request값 받은거를 넘긴다.
         ViewDetailResponse response = buildingService.viewDetailService(request);
         return ResponseEntity.ok(response);
     }
