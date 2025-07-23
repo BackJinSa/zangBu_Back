de986db8a1effa5853c2b4e16d37677195dbdcbe
Suyeon An <129sy@naver.com>
2025-07-23 15:17:30 +0900

    중간 commit : 파일 합침

--- src/main/java/bjs/zangbu/chat/dto/ChatResponse.java
+++ src/main/java/bjs/zangbu/chat/dto/ChatResponse.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
-public class ChatMessageResponse {
+public class ChatResponse {
 
     // /chat/room/{roomId}?lastMessageId={lastMessageId}&limit={limit}
-    public static class ChatMessageListResponse { //roomId에 해당하는 채팅방의 메시지들 마지막부터 limit개 불러오기
+    public static class ChatMessageListResponse {
+        //roomId에 해당하는 채팅방의 메시지들 마지막부터 limit개 불러오기
         private String buildingName;    //건물 이름
         private String sellerNickname;  //판매자 닉네임
         private String sellerType;      //세입자, 집주인
@@ -34,5 +37,38 @@ public class ChatMessageResponse {
         private String message;          //보낸 메시지 내용
         private String sendNickname;     //보낸 사람 닉네임
         private String createdAt;        //메시지 보낸 시간
+    }
+
+    //     /chat/room/enter/{roomId}
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class EnterChatRoomResponse {
+        //이미 존재하는 채팅방의 상세정보를 응답할 때 사용
+        private String chatRoomId;         //채팅방 id
+        private Long buildingId;           //매물 id
+        private String buyerId;            //구매자 id
+        private String sellerNickname;     //판매자 닉네임
+        private LocalDateTime createdAt;   //메시지 생성 시각
+    }
+
+
+    //    /chat/list?page={page}&size={size}&type={type}
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class ChatRoomListDto {
+        private String chatRoomId;      //채팅방 id
+        private String buildingName;    //매물 이름
+        private String lastMessage;     //해당 채팅방의 마지막 메시지
+        private String lastMessageTime;     //해당 채팅방의 마시막 메시지 보낸 시각
+        private String buyerNickname;       //구매자 닉네임
+        private String sellerNickname;      //판매자 닉네임
+        private String status;              //거래 상태
+        private String sellerType;          //판매자 타입 : 집주인 or 세입자
+        private boolean hasNext;            //페이지네이션 다음 여부
+
     }
 }
--- src/main/java/bjs/zangbu/chat/dto/request/ChatRequest.java
+++ src/main/java/bjs/zangbu/chat/dto/request/ChatRequest.java
@@ -5,18 +5,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-public class ChatMessageRequest {
+public class ChatRequest {
 
     //   /chat/room/{roomId}?lastMessageId={lastMessageId}&limit={limit}
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class ChatMessageListRequest { //roomId에 해당하는 채팅방의 메시지들 마지막부터 limit개 불러오기
+    public static class ChatMessageListRequest {
+        //roomId에 해당하는 채팅방의 메시지들 마지막부터 limit개 불러오기
         private Long lastMessageId;  // 마지막으로 받은 메시지 ID
         private int limit = 20;      // 기본값 20
         //컨트롤러에서 @RequestParam으로
     }
-
 
     //  /pub/chat.message
     @Getter
@@ -26,5 +26,15 @@ public class ChatMessageRequest {
     public static class SendMessageRequest {
         private String chatRoomId;   //채팅방 ID
         private String message;      //보낼 메시지 내용
+    }
+
+    //  /chat/list?page={page}&size={size}&type={type}
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class ChatRoomListRequest {
+        private int page;       //페이지
+        private int size;       //한 페이지에 보여줄 채팅방 개수
+        private String type; // "BUYER" or "SELLER"
     }
 }
