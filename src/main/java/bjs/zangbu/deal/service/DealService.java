8b4a485823f2ff8b6276c9d22699282a5c675d1a
saranghein <saranghein@gmail.com>
2025-07-23 17:40:18 +0900

    feat:service 메서드 추가
    (#22)

--- src/main/java/bjs/zangbu/deal/service/DealService.java
+++ src/main/java/bjs/zangbu/deal/service/DealService.java
@@ -1,5 +1,20 @@
 package bjs.zangbu.deal.service;
 
+import bjs.zangbu.deal.dto.response.DealResponse.Notice;
+import bjs.zangbu.deal.dto.response.DealWaitingListResponse.WaitingList;
+import bjs.zangbu.deal.dto.response.DealWaitingListResponse.WaitingListElement;
+import bjs.zangbu.deal.dto.response.DealWaitingListResponse.WaitingListPurchase;
+import java.util.List;
+
 public interface DealService {
 
+  Notice getNotice(Long buildingId);
+
+  WaitingList getAllWaitingList(String userId, String nickname);
+
+  WaitingListPurchase getPurchaseWaitingList(String userId);
+
+  List<WaitingListElement> getOnSaleWaitingList(String userId);
+
+  boolean deleteDealById(Long dealId);
 }
