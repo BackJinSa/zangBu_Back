88cd9dd6f65b1cd57a0326f701641d0a228e17a8
LeeHaeIn <98319061+saranghein@users.noreply.github.com>
2025-07-24 13:11:00 +0900

    feat: DealService 구현 (#55)
    ## 🔥 이슈
    
    - #22 
    
    ## 📜 개요
    
    - getPurchaseWaitingList, getOnSaleWaitingList 구현
    - deleteDealById 구현
    - patchStatus 구현
    - DealMapper 구현
    - 추가 메서드 및 intent 관련 서비스는 분석 리포트 끝나면 조회가 필요하므로 추후 개발하겠습니다.
    
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [x] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [ ] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [x] 코드 리팩토링
    - [ ] 주석 추가 및 수정
    - [ ] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [x] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [ ] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).

--- src/main/java/bjs/zangbu/deal/mapper/DealMapper.java
+++ src/main/java/bjs/zangbu/deal/mapper/DealMapper.java
@@ -1,6 +1,7 @@
 package bjs.zangbu.deal.mapper;
 
-import bjs.zangbu.deal.vo.Deal;
+import bjs.zangbu.deal.dto.join.DealWithChatRoom;
+import bjs.zangbu.deal.dto.request.DealRequest.Status;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -9,6 +10,9 @@ public interface DealMapper {
 
   List<DealWithChatRoom> getWaitingDealsWithChatRoom(String userId);
 
+  int deleteDealById(Long dealId);
 
-  List<Deal> getAllDealByUserId(String userId);
+  int patchStatus(Status status);
+
+  String getStatusByDealId(Long dealId);
 }
--- src/main/java/bjs/zangbu/deal/vo/DealEnum.java
+++ src/main/java/bjs/zangbu/deal/vo/DealEnum.java
@@ -1,9 +1,16 @@
 package bjs.zangbu.deal.vo;
 
 public enum DealEnum {
-     BEFORE_TRANSACTION, // 거래 전
-    BEFORE_OWNER, // 판매자 수락 전
-    BEFORE_CONSUMER, // 구매자 수락 전
-    MIDDLE_DEAL, // 거래 중
-    CLOSE_DEAL // 거래 성사
+
+  // [거래 전] -> 채팅 시작 ->
+  // [판매자 수락 전] -> 판매자 수락 ->
+  // [구매자 수락 전] -> 구매자 수락 ->
+  // [거래 중] -> 거래 완료 ->
+  // [거래 성사]
+
+  BEFORE_TRANSACTION, // 거래 전
+  BEFORE_OWNER, // 판매자 수락 전
+  BEFORE_CONSUMER, // 구매자 수락 전
+  MIDDLE_DEAL, // 거래 중
+  CLOSE_DEAL // 거래 성사
 }
