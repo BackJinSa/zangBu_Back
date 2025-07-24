ccac9d01adcf048e8581bdaba6308465d2962dbb
Jeon, Kyunghwan <114416829+JJ-HMFIC@users.noreply.github.com>
2025-07-24 13:50:00 +0900

    Feature/deal report (#59)
    ## 🔥 이슈
    
    <!--- -# 이슈번호 적으면 됩니다. -->
    #7 
    
    ## 📜 개요
    
    <!---- 변경 사항 및 관련 이슈에 대해 간단하게 작성하면됩니다. 
           어떻게보다 무엇을 왜 수정했는지 설명해주세요. -->
    1. 표준 계약서 파일 추가
    2. 표준 계약서 관련 서비스 계층 분리(ContractService)
    3. 컨트롤러 코드 추가
    4. mapper 추가 및 수정, 병합
    5. 테스트 코드 추가(user 생성되면 테스트 해볼 예정)
    <!---- Resolves: #(Isuue Number) -->
    #7 
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [x] 새로운 기능 추가
    - [x] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [ ] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [ ] 코드 리팩토링
    - [ ] 주석 추가 및 수정
    - [ ] 문서 수정
    - [x] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [x] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).

--- src/main/java/bjs/zangbu/deal/mapper/DealMapper.java
+++ src/main/java/bjs/zangbu/deal/mapper/DealMapper.java
@@ -1,6 +1,9 @@
 package bjs.zangbu.deal.mapper;
 
 import bjs.zangbu.deal.dto.join.DealWithChatRoom;
+import bjs.zangbu.deal.dto.join.DealWithSaleType;
+import bjs.zangbu.deal.vo.Deal;
+import bjs.zangbu.deal.dto.join.DealWithChatRoom;
 import bjs.zangbu.deal.dto.request.DealRequest.Status;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
@@ -15,4 +18,6 @@ public interface DealMapper {
   int patchStatus(Status status);
 
   String getStatusByDealId(Long dealId);
+
+  DealWithSaleType findWithType(Long dealId);
 }
