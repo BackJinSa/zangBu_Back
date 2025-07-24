c41dd7fc268b4b315e91e8f3aa8eca6437c8a395
KyungHwan Jeon <crasheso@gmail.com>
2025-07-24 11:44:59 +0900

    Feat : 표준계약서 로직 추가

--- src/main/java/bjs/zangbu/deal/mapper/DealMapper.java
+++ src/main/java/bjs/zangbu/deal/mapper/DealMapper.java
@@ -1,6 +1,9 @@
 package bjs.zangbu.deal.mapper;
 
 import bjs.zangbu.deal.dto.join.DealWithChatRoom;
+import bjs.zangbu.deal.dto.join.DealWithSaleType;
+import bjs.zangbu.deal.vo.Deal;
+import bjs.zangbu.deal.dto.join.DealWithChatRoom;
 import bjs.zangbu.deal.dto.request.DealRequest.Status;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
@@ -15,4 +18,7 @@ public interface DealMapper {
   int patchStatus(Status status);
 
   String getStatusByDealId(Long dealId);
+  List<Deal> getAllDealByUserId(String userId);
+
+  DealWithSaleType findWithType(Long dealId);
 }
