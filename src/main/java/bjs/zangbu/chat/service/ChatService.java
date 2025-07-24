2e4d8abc15935ebd9188eaab42497517d607ba87
Suyeon An <129sy@naver.com>
2025-07-24 17:41:22 +0900

    pull 하기 위한 commit

--- src/main/java/bjs/zangbu/chat/service/ChatService.java
+++ src/main/java/bjs/zangbu/chat/service/ChatService.java
@@ -1,4 +1,30 @@
 package bjs.zangbu.chat.service;
 
+import bjs.zangbu.chat.vo.ChatMessage;
+import bjs.zangbu.chat.vo.ChatRoom;
+
+import java.util.List;
+
 public interface ChatService {
+
+    //메시지 전송
+    void sendMessage(ChatMessage message);
+
+    //chatRoomId로 해당 채팅방의 메시지들 limit개 불러오기
+    List<ChatMessage> getMessages(String chatRoomId, Long lastMessageId, int limit);
+
+    //chatRoomId로 해당 채팅방의 상세정보 가져오기
+    ChatRoom getChatRoomDetail(String chatRoomId);
+
+    //userId를 기준으로 사용자가 참여한 채팅방 리스트 가져옴, page랑 size는 페이지네이션용
+    List<ChatRoom> getChatRoomList(String userId, String type, int page, int size);
+
+    //채팅방 중복 생성 방지를 위해 채팅방 존재 유무 확인
+    boolean existsChatRoom(Long buildingId, String consumerId);
+
+    //채팅방 생성
+    void createChatRoom(ChatRoom chatRoom);
+
+    //채팅방 삭제
+    void deleteChatRoom(String chatRoomId);
 }
--- src/main/java/bjs/zangbu/chat/service/ChatServiceImpl.java
+++ src/main/java/bjs/zangbu/chat/service/ChatServiceImpl.java
@@ -1,4 +1,57 @@
 package bjs.zangbu.chat.service;
 
+import bjs.zangbu.chat.mapper.ChatMapper;
+import bjs.zangbu.chat.vo.ChatMessage;
+import bjs.zangbu.chat.vo.ChatRoom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ChatServiceImpl implements ChatService{
+
+    private final ChatMapper chatMapper;
+
+    @Override
+    public void sendMessage(ChatMessage message) {
+        chatMapper.insertMessage(message);
+    }
+
+    @Override
+    public List<ChatMessage> getMessages(String chatRoomId, Long lastMessageId, int limit) {
+        return chatMapper.selectMessagesByRoomId(chatRoomId, lastMessageId, limit);
+    }
+
+    @Override
+    public ChatRoom getChatRoomDetail(String chatRoomId) {
+        return chatMapper.selectChatRoomById(chatRoomId);
+    }
+
+    @Override
+    public List<ChatRoom> getChatRoomList(String userId, String type, int page, int size) {
+        int offset = (page - 1) * size;
+        return chatMapper.selectChatRoomList(userId, type, offset, size);
+    }
+
+    @Override
+    public boolean existsChatRoom(Long buildingId, String consumerId) {
+        return chatMapper.existsChatRoom(buildingId, consumerId);
+    }
+
+    @Override
+    public void createChatRoom(ChatRoom chatRoom) {
+        chatMapper.insertChatRoom(chatRoom);
+    }
+
+    @Override
+    @Transactional
+    public void deleteChatRoom(String chatRoomId) {
+        //chatRoomId의 ChatMessage들 삭제
+        chatMapper.deleteMessagesByRoomId(chatRoomId);
+        //chatRoomId의 ChatRoom 삭제
+        chatMapper.deleteChatRoom(chatRoomId);
+    }
 }
