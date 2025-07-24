380f6a04e8e0afe827d38df8854bcfa962e466de
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-07-24 15:05:06 +0900

    feat:imageListService 기능 개발
    (#62)

--- src/main/java/bjs/zangbu/imageList/service/ImageListService.java
+++ src/main/java/bjs/zangbu/imageList/service/ImageListService.java
@@ -7,4 +7,7 @@ public interface ImageListService {
 
     // 이미지 정보 관련 서비스 인터페이스
     void createImageList(ImageList imageList);
+
+    // 대표 이미지 가져오기
+    String representativeImage(Long buildingId);
 }
