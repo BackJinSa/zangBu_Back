769c4c39a56ae62c5e000a1b655953b978354c9b
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-07-25 14:08:38 +0900

    feat:codef 기능 개발 (#82)
    ## 🔥 이슈
    - #1
    <!--- -# 이슈번호 적으면 됩니다. -->
    
    ## 📜 개요
    
    <!---- 변경 사항 및 관련 이슈에 대해 간단하게 작성하면됩니다. 
           어떻게보다 무엇을 왜 수정했는지 설명해주세요. -->
    
    <!---- Resolves: #(Isuue Number) -->
    
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [x] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [ ] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [x] 코드 리팩토링
    - [x] 주석 추가 및 수정
    - [ ] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [x] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [ ] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [ ] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).

---
+++ src/main/java/bjs/zangbu/codef/thread/CodefThread.java
@@ -0,0 +1,73 @@
+package bjs.zangbu.codef.thread;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.codef.api.EasyCodef;
+import io.codef.api.EasyCodefServiceType;
+
+import java.util.HashMap;
+
+
+public class CodefThread extends Thread {
+    private EasyCodef codef;
+    private HashMap<String, Object> parameterMap;
+
+    public int getThreadNo() {
+        return threadNo;
+    }
+
+    private int threadNo;
+    private String productUrl;
+
+    // 2-Way 인증 정보
+    private Integer jobIndex;
+    private Integer threadIndex;
+    private String jti;
+    private Long twoWayTimestamp;
+    private String firstResponse;
+    private String secondResponse;
+
+    public CodefThread(EasyCodef codef, HashMap<String, Object> parameterMap, int threadNo, String productUrl) {
+        this.codef = codef;
+        this.parameterMap = parameterMap;
+        this.threadNo = threadNo;
+        this.productUrl = productUrl;
+    }
+
+    public String getFirstResponse() { return firstResponse; }
+    public String getSecondResponse() { return secondResponse; }
+
+    @Override
+    public void run() {
+        try {
+            // 1차 인증 요청
+            firstResponse = codef.requestProduct(productUrl, EasyCodefServiceType.DEMO, parameterMap);
+
+            HashMap<String, Object> responseMap = new ObjectMapper().readValue(firstResponse, HashMap.class);
+            HashMap<String, Object> resultMap = (HashMap<String, Object>) responseMap.get("result");
+            String code = (String) resultMap.get("code");
+            HashMap<String, Object> dataMap = (HashMap<String, Object>) responseMap.get("data");
+
+            boolean continue2Way = false;
+            if (dataMap != null && dataMap.containsKey("continue2Way")) {
+                continue2Way = Boolean.TRUE.equals(dataMap.get("continue2Way"));
+            }
+
+            // 2-Way 인증이 필요하면 값 저장 → 즉시 2차 인증 요청
+            if ("CF-03002".equals(code) && continue2Way) {
+                this.jobIndex = (Integer) dataMap.get("jobIndex");
+                this.threadIndex = (Integer) dataMap.get("threadIndex");
+                this.jti = (String) dataMap.get("jti");
+                this.twoWayTimestamp = ((Number) dataMap.get("twoWayTimestamp")).longValue();
+
+                // 2차 인증 호출
+                HashMap<String, Object> twoWayParams = new HashMap<>();
+                twoWayParams.put("jobIndex", jobIndex);
+                twoWayParams.put("threadIndex", threadIndex);
+                twoWayParams.put("jti", jti);
+                twoWayParams.put("twoWayTimestamp", twoWayTimestamp);
+                secondResponse = codef.requestProduct(productUrl, EasyCodefServiceType.DEMO, twoWayParams);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
