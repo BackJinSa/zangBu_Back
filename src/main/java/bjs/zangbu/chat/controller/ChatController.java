e64d224f4215a9fcaa259eee14e06be0e32c2e84
Suyeon An <129sy@naver.com>
2025-07-25 11:39:23 +0900

    pull commit

--- src/main/java/bjs/zangbu/chat/controller/ChatController.java
+++ src/main/java/bjs/zangbu/chat/controller/ChatController.java
@@ -1,4 +1,18 @@
 package bjs.zangbu.chat.controller;
 
+import bjs.zangbu.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat")
 public class ChatController {
+
+    private final ChatService chatService;
+
+
+
+
 }
---
+++ src/main/java/bjs/zangbu/chat/controller/ChatWebSocketController.java
@@ -0,0 +1,4 @@
+package bjs.zangbu.chat.controller;
+
+public class ChatWebSocketController {
+}
