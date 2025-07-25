781e810725191b94487f9736ef9e1c7fb8c93692
zefa42 <youngoh07015@gmail.com>
2025-07-25 10:58:18 +0900

    feat: review 목록 불러오는 기능 mapper 구현

---
+++ src/main/java/bjs/zangbu/review/mapper/ReviewMapper.java
@@ -0,0 +1,21 @@
+package bjs.zangbu.review.mapper;
+
+import bjs.zangbu.review.dto.response.ReviewListResponse;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ReviewMapper {
+    List<ReviewListResponse> selectByBuilding(
+            @Param("buildingId") long buildingId,
+            @Param("offset") int offset,
+            @Param("limit") int limit
+    );
+
+    Long countByBuilding(@Param("building") Long buildingId);
+
+    // 최신 리뷰의 별점 가져오기
+    Integer selectLatestReviewRank(@Param("buildingId") Long buildingId);
+}
