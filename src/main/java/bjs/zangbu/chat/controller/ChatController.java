4b41efcd52b75b66023e3b3ca2324e5b0e8acf48
Suyeon An <129sy@naver.com>
2025-07-25 16:12:57 +0900

    feat: chat controller 작성 ChatController, ChatWebSocketController 작성 (#80)

--- src/main/java/bjs/zangbu/chat/controller/ChatController.java
+++ src/main/java/bjs/zangbu/chat/controller/ChatController.java
@@ -1,9 +1,12 @@
 package bjs.zangbu.chat.controller;
 
 import bjs.zangbu.chat.service.ChatService;
+import bjs.zangbu.chat.vo.ChatMessage;
+import bjs.zangbu.chat.vo.ChatRoom;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,7 +15,41 @@ public class ChatController {
 
     private final ChatService chatService;
 
+    // 채팅방 생성 후 반환
+    // 이미 채팅방 존재하면 그 채팅방 반환
+    @PostMapping("/room")
+    public ChatRoom createChatRoom(@RequestBody ChatRoom chatRoom) {
+        return chatService.createChatRoom(chatRoom);
+    }
 
+    // 사용자가 참여하고 있는 채팅방 목록 조회
+    @GetMapping("/list")
+    public List<ChatRoom> getChatRoomList(
+            @RequestParam String userId, @RequestParam String type,
+            @RequestParam int page, @RequestParam int size) {
+        return chatService.getChatRoomList(userId, type, page, size);
+    }
 
+    // 채팅방 상세 정보 조회
+    @GetMapping("/room/{roomId}")
+    public ChatRoom getChatRoomDetail(@PathVariable String roomId) {
+        return chatService.getChatRoomDetail(roomId);
+    }
+
+    //채팅방의 메시지들 조회 (한 번에 보여주는 개수는 기본 20개)
+    @GetMapping("/room/{roomId}/messages")
+    public List<ChatMessage> getMessages(
+            @PathVariable String roomId,
+            @RequestParam(required = false) Long lastMessageId,
+            @RequestParam(defaultValue = "20") int limit
+    ) {
+        return chatService.getMessages(roomId, lastMessageId, limit);
+    }
+
+    // 채팅방 삭제
+    @DeleteMapping("/room/{roomId}")
+    public void deleteChatRoom(@PathVariable String roomId) {
+        chatService.deleteChatRoom(roomId);
+    }
 
 }
