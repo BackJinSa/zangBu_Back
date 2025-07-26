29db6950a19e2d4c66066a7fd9f88deffd7286f8
saranghein <saranghein@gmail.com>
2025-07-26 09:34:30 +0900

    chore: LocalDateFormatter 적용 및 미사용 클래스 제거
    - DocumentReport DTO 에 LocalDateFormatter 적용
    - 미사용 Deal DTO 내 Report 클래스 제거
    
    (#86)

--- src/main/java/bjs/zangbu/documentReport/dto/response/DocumentReportResponse.java
+++ src/main/java/bjs/zangbu/documentReport/dto/response/DocumentReportResponse.java
@@ -1,7 +1,7 @@
 package bjs.zangbu.documentReport.dto.response;
 
 import bjs.zangbu.documentReport.vo.DocumentReport;
-import java.time.LocalDateTime;
+import bjs.zangbu.global.formatter.LocalDateFormatter.CreatedAt;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,21 +24,21 @@ public class DocumentReportResponse {
   @NoArgsConstructor
   public static class DocumentReportElement {
 
-    Long reportId;           // 분석 리포트 식별 id
-    Long buildingId;         // 매물 식별 id
-    Long complexId;          // 단지 식별 id
-    String commUniqueNo;     // 부동단 고유 식별 번호
-    int dealAmount;          // 거래 금액(만원)
-    int deposit;             // 보증 금액(만원)
-    int monthlyRent;         // 월세 금액(만원)
-    int priorityDept;        // 선순위 채권액
-    int depositPrice;        // 내 보증금
-    int finalAuctionPrice;   // 최종 낙찰가
-    int remainingDeposit;    // 여유 보증금
-    String resUserNum;       // 소유자 정보
-    boolean isTrustee;       // 신탁 여부
-    String trustType;        // 신탁 내용 텍스트
-    LocalDateTime createdAt; // 보고서 생성 시간
+    Long reportId;         // 분석 리포트 식별 id
+    Long buildingId;       // 매물 식별 id
+    Long complexId;        // 단지 식별 id
+    String commUniqueNo;   // 부동단 고유 식별 번호
+    int dealAmount;        // 거래 금액(만원)
+    int deposit;           // 보증 금액(만원)
+    int monthlyRent;       // 월세 금액(만원)
+    int priorityDept;      // 선순위 채권액
+    int depositPrice;      // 내 보증금
+    int finalAuctionPrice; // 최종 낙찰가
+    int remainingDeposit;  // 여유 보증금
+    String resUserNum;     // 소유자 정보
+    boolean isTrustee;     // 신탁 여부
+    String trustType;      // 신탁 내용 텍스트
+    String createdAt;      // 보고서 생성 시간
 
     public static DocumentReportElement toDto(DocumentReport vo) {
       return new DocumentReportElement(
@@ -56,7 +56,7 @@ public class DocumentReportResponse {
           vo.getResUserNum(),
           vo.isTrustee(),
           vo.getTrustType(),
-          vo.getCreatedAt()
+          CreatedAt.formattingCreatedAt(vo.getCreatedAt())
       );
     }
   }
