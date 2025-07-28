f3ba0dc9981b551929a788a6d696a08c28987b0b
zefa42 <youngoh07015@gmail.com>
2025-07-28 10:54:41 +0900

    feat: payment 결제 승인 요청 기능 mapper 구현

--- src/main/java/bjs/zangbu/payment/mapper/PaymentMapper.java
+++ src/main/java/bjs/zangbu/payment/mapper/PaymentMapper.java
@@ -1,8 +1,10 @@
 package bjs.zangbu.payment.mapper;
 
+import bjs.zangbu.payment.dto.request.PaymentConfirmRequest;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PaymentMapper {
     int insertPayment(PaymentInsertParam param);
+    int confirmPayment(PaymentConfirmRequest req);
 }
