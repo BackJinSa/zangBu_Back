3e397ef803f5d5378a85659d45de1d5e5057677c
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-07-29 10:47:14 +0900

    style: 주석 수정 (#134)
    ## 🔥 이슈
    - #133 
    <!--- -# 이슈번호 적으면 됩니다. -->
    
    ## 📜 개요
    주석 다 달았고 매퍼 필요하다고 한거 서비스에서 쓸수 있게 해놨습니다. 
    빌딩 필터링된 리스트 가져오는거 PageHelper쓰는걸로 리팩토링 진행했습니다.
    <!---- 변경 사항 및 관련 이슈에 대해 간단하게 작성하면됩니다. 
           어떻게보다 무엇을 왜 수정했는지 설명해주세요. -->
    
    <!---- Resolves: #(Isuue Number) -->
    
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [x] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [x] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [x] 코드 리팩토링
    - [x] 주석 추가 및 수정
    - [ ] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [x] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).

--- src/main/java/bjs/zangbu/complexList/service/ComplexListService.java
+++ src/main/java/bjs/zangbu/complexList/service/ComplexListService.java
@@ -2,8 +2,16 @@ package bjs.zangbu.complexList.service;
 
 import bjs.zangbu.complexList.vo.ComplexList;
 
+/**
+ * 복합 단지(ComplexList) 관련 서비스 인터페이스
+ */
 public interface ComplexListService {
 
-    // 단지 정보 등록 후 생성된 ID 반환
+    /**
+     * 단지 정보를 등록하고 생성된 ID를 반환합니다.
+     *
+     * @param complexList 등록할 단지 정보 객체
+     * @return 등록 후 생성된 단지 ID
+     */
     Long createComplexList(ComplexList complexList);
 }
--- src/main/java/bjs/zangbu/imageList/service/ImageListServiceImpl.java
+++ src/main/java/bjs/zangbu/imageList/service/ImageListServiceImpl.java
@@ -1,22 +1,36 @@
 package bjs.zangbu.imageList.service;
+
 import bjs.zangbu.imageList.mapper.ImageListMapper;
 import bjs.zangbu.imageList.vo.ImageList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * 이미지 관련 비즈니스 로직을 처리하는 서비스 구현체
+ */
 @Service
 @RequiredArgsConstructor
 public class ImageListServiceImpl implements ImageListService {
 
+    /** 이미지 매퍼 객체 주입 */
     private final ImageListMapper imageListMapper;
 
-    // 이미지 정보 등록 처리
+    /**
+     * 이미지 정보를 등록 처리합니다.
+     *
+     * @param imageList 등록할 이미지 정보 객체
+     */
     @Override
     public void createImageList(ImageList imageList) {
         imageListMapper.createImageList(imageList);
     }
 
-    // 대표 이미지 가져오기
+    /**
+     * 특정 건물의 대표 이미지 URL을 조회합니다.
+     *
+     * @param buildingId 조회할 건물 ID
+     * @return 대표 이미지 URL 문자열
+     */
     @Override
     public String representativeImage(Long buildingId) {
         return imageListMapper.representativeImage(buildingId);
