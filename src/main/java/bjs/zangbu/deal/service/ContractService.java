47809b6e171040deb05904cd2b8c8f459b3fd76c
KyungHwan Jeon <crasheso@gmail.com>
2025-07-29 11:26:23 +0900

    feat : 건축물대장 로직 추가 및 수정

--- src/main/java/bjs/zangbu/deal/service/ContractService.java
+++ src/main/java/bjs/zangbu/deal/service/ContractService.java
@@ -1,5 +1,8 @@
 package bjs.zangbu.deal.service;
 
+import bjs.zangbu.deal.dto.response.BuildingRegisterResponse;
+
 public interface ContractService {
     public String getContractPdf(Long dealId);
+    BuildingRegisterResponse generateRegisterPdf(Long dealId) throws Exception;
 }
