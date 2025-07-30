d073f464a06f01ff6905d250f672bcbd0c5a175e
zefa05 <youngoh07015@gmail.com>
2025-07-30 12:50:09 +0900

    feat: 공공데이터 api 추가-단지 목록-dto 구현

---
+++ src/main/java/bjs/zangbu/publicdata/dto/aptlist/AptComplex.java
@@ -0,0 +1,16 @@
+package bjs.zangbu.publicdata.dto.aptlist;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AptComplex {
+    private String kaptCode;
+    private String kaptName;
+    private String bjdCode;
+    private String as1;
+    private String as2;
+    private String as3;
+    private String as4;
+}
