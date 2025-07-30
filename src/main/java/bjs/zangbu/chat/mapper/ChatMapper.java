9e3b35c2436e4a84977d9330cb92649e7f32fcaa
suyeonan <88480976+suyeonan@users.noreply.github.com>
2025-07-30 14:20:20 +0900

    feat: chat swagger 추가 (#163)
    ## 🔥 이슈
    
    #151 
    
    ## 📜 개요
    
    chat에 swagger 추가
    
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [ ] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [ ] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [ ] 코드 리팩토링
    - [x] 주석 추가 및 수정
    - [x] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [ ] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).

--- src/main/java/bjs/zangbu/chat/mapper/ChatMapper.java
+++ src/main/java/bjs/zangbu/chat/mapper/ChatMapper.java
@@ -6,13 +6,14 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface ChatMapper {
 
     //메시지 전송 시 DB에 메시지 저장
     int insertMessage(ChatMessage chatMessage);
 
     //chatRoomId에 해당하는 메시지들 조회(제일 마지막 메시지부터 limit개 -> 더보기 클릭 시 limit개씩 추가 조회)
-    List<ChatMessage> selectMessagesByRoomId(String chatRoomId, Long lastMessageId, int limit);
+    List<ChatMessage> selectMessagesByRoomId(String chatRoomId, int limit);
 
     //chatRoomId에 해당하는 채팅방 상세 조회
     ChatRoom selectChatRoomById(String chatRoomId);
