3610a1d9aa91b27c51d06b0c6c197d488e93c80b
zefa05 <youngoh07015@gmail.com>
2025-07-30 09:53:56 +0900

    feat: 공공데이터 api 추가-개별 관리비-dto 구현

---
+++ src/main/java/bjs/zangbu/publicdata/dto/managecost/GasCost.java
@@ -0,0 +1,13 @@
+package bjs.zangbu.publicdata.dto.managecost;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GasCost {
+    private String kaptCode;
+    private String kaptName;
+    private Long gasC;  // 세대별 가스 사용요금 총합
+    private Long gasP;  // 전체 가스 공급사 납부 총비용
+}
