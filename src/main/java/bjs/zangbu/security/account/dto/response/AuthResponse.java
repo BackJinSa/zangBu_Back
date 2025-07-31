85d7e7becf7d2c196aa36c91e98d9510d6f9d977
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-07-31 10:06:39 +0900

    docs: member, auth swagger 추가 (#170)
    ## 🔥 이슈
    
    #162 
    
    ## 📜 개요
    
    member, auth swagger 생성 및 authService 리팩토링
    
    <!---- Resolves: #(Isuue Number) -->
    
    ## 🤔 PR 유형
    
    어떤 변경 사항이 있나요?
    
    - [ ] 새로운 기능 추가
    - [ ] 버그 수정
    - [ ] CSS 등 사용자 UI 디자인 변경
    - [ ] 코드에 영향을 주지 않는 변경사항(오타 수정, 탭 사이즈 변경, 변수명 변경)
    - [x] 코드 리팩토링
    - [x] 주석 추가 및 수정
    - [x] 문서 수정
    - [ ] 테스트 추가, 테스트 리팩토링
    - [ ] 빌드 부분 혹은 패키지 매니저 수정
    - [ ] 파일 혹은 폴더명 수정
    - [ ] 파일 혹은 폴더 삭제
    
    ## ✅ PR Checklist
    
    PR이 다음 요구 사항을 충족하는지 확인하세요.
    
    - [x] 커밋 메시지 컨벤션에 맞게 작성했습니다.
    - [ ] 변경 사항에 대한 테스트를 했습니다.(버그 수정/기능에 대한 테스트).
    
    ---------
    
    Co-authored-by: M6F\student <sojsnake@gmail.com>

--- src/main/java/bjs/zangbu/security/account/dto/response/AuthResponse.java
+++ src/main/java/bjs/zangbu/security/account/dto/response/AuthResponse.java
@@ -1,6 +1,7 @@
 package bjs.zangbu.security.account.dto.response;
 
 import bjs.zangbu.security.account.vo.MemberEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,19 @@ public class AuthResponse {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "LoginResponse", description = "로그인 응답 DTO")
     public static class LoginResponse{
+        @Schema(description = "액세스 토큰",
+                example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyQGV4YW1wbGUuY29tIn0.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
         private String accessToken;
+
+        @Schema(description = "리프레시 토큰",
+                example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJyZWZyZXNoIn0.4IVKn6X2OeTYuQUbPW-QgLkekJQZkp7pJKG_LMWMNoY")
         private String refreshToken;
+
+        @Schema(description = "역할", example = "ROLE_MEMBER", allowableValues = {
+                "ROLE_MEMBER", "ROLE_ADMIN"
+        })
         private MemberEnum role;
     }
 
@@ -21,7 +32,9 @@ public class AuthResponse {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "EmailAuthResponse", description = "이메일 찾기 응답 DTO")
     public static class EmailAuthResponse{
+        @Schema(description = "이메일", example = "example.zangbu.com")
         private String email;
     }
 
@@ -29,8 +42,12 @@ public class AuthResponse {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "AuthVerify", description = "본인인증 결과 응답 DTO")
     public static class AuthVerify{
+        @Schema(description = "진위확인 결과", example = "Y")
         private String resAuthenticity; // 진위확인 결과 (ex: "Y", "N")
+
+        @Schema(description = "진위확인 내용", example = "성공")
         private String resAuthenticityDesc; // 진위확인 내용 (ex: "성공", "주민번호 불일치")
     }
 
@@ -38,8 +55,14 @@ public class AuthResponse {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "TokenResponse", description = "토큰 재발급 응답 DTO")
     public static class TokenResponse{
+        @Schema(description = "액세스 토큰",
+                example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyQGV4YW1wbGUuY29tIn0.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
         private String accessToken;
+
+        @Schema(description = "리프레시 토큰",
+                example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJyZWZyZXNoIn0.4IVKn6X2OeTYuQUbPW-QgLkekJQZkp7pJKG_LMWMNoY")
         private String refreshToken;
     }
 }
