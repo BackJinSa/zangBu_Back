8be8fa1eaa9e628ab80c41a01290910206e980fe
saranghein <saranghein@gmail.com>
2025-08-14 17:04:26 +0900

    chore: path variable 수정
    (#247)

--- src/main/java/bjs/zangbu/deal/service/ContractServiceImpl.java
+++ src/main/java/bjs/zangbu/deal/service/ContractServiceImpl.java
@@ -1,5 +1,7 @@
 package bjs.zangbu.deal.service;
 
+import static bjs.zangbu.ncp.auth.Holder.HeaderCreationHolder.BUCKET_NAME;
+
 import bjs.zangbu.codef.converter.CodefConverter;
 import bjs.zangbu.codef.service.CodefService;
 import bjs.zangbu.codef.service.CodefTwoFactorService;
@@ -19,79 +21,79 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static bjs.zangbu.ncp.auth.Holder.HeaderCreationHolder.BUCKET_NAME;
-
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class ContractServiceImpl implements ContractService {
-    private final DealMapper dealMapper;
-    private final CodefService codefService;
-    private final CodefTwoFactorService codefTwoFactorService;
-    private final BinaryUploaderService binaryUploaderService;
+
+  private final DealMapper dealMapper;
+  private final CodefService codefService;
+  private final CodefTwoFactorService codefTwoFactorService;
+  private final BinaryUploaderService binaryUploaderService;
 
 
-    @Override
-    @Transactional(readOnly = true)
-    /** dealId → "/contracts/xxx.pdf" 상대 경로 반환 */
-    public String getContractPdf(Long dealId) {
-        DealWithSaleType deal = dealMapper.findWithType(dealId);
+  @Override
+  @Transactional(readOnly = true)
+  /** dealId → "/contracts/xxx.pdf" 상대 경로 반환 */
+  public String getContractPdf(Long dealId) {
+    DealWithSaleType deal = dealMapper.findWithType(dealId);
 
-        if(deal == null) {
-            throw new IllegalArgumentException("해당 거래가 존재하지 않습니다 dealId: "+ dealId);
-        }
-
-        String path;
-        SaleType type = deal.getSaleType(); // 매매 전월세 타입 분류
-
-        if(type == SaleType.TRADING){ // 매매
-            path = "/contracts/sale_contract.pdf";
-        } else if (type == SaleType.MONTHLY || type == SaleType.CHARTER) {
-            // 전월세
-            path = "/contracts/lease_contract.pdf";
-        } else{
-            throw new IllegalStateException("거래 타입 오류, saleType : " + type);
-        }
-        return path;
+    if (deal == null) {
+      throw new IllegalArgumentException("해당 거래가 존재하지 않습니다 dealId: " + dealId);
     }
 
-    // 등기부등본 발급 api
-    @Override
-    public DealResponse.Download getEstateRegisternPdf(Long dealId)
-            throws Exception {
-        //DB에서 데이터 가져와서 request 생성
-        EstateRegistrationRequest request = dealMapper.getEstateRegistrationRequest(dealId);
-        // codef에서 응답 가져오기
-        String rawResponse = codefService.realEstateRegistrationLeader(request);
-        // pdf dto 파싱로직
-        EstateRegistrationResponse dto = CodefConverter.parseDataToDto(
-                rawResponse, EstateRegistrationResponse.class);
-        // PDF 바이트 추출
-        byte[] pdfBytes = PdfUtil.decodePdfBytes(dto.getResOriGinalData());
-        /* 6) ncp 업로드 */
-        String key  = "estate-Register/" + dealId + ".pdf";
-        String url = binaryUploaderService.putPdfObject(BUCKET_NAME,key,pdfBytes);
+    String path;
+    SaleType type = deal.getSaleType(); // 매매 전월세 타입 분류
 
-        return new DealResponse.Download(url);
+    if (type == SaleType.TRADING) { // 매매
+      path = "/contracts/sale_contract.pdf";
+    } else if (type == SaleType.MONTHLY || type == SaleType.CHARTER) {
+      // 전월세
+      path = "/contracts/lease_contract.pdf";
+    } else {
+      throw new IllegalStateException("거래 타입 오류, saleType : " + type);
     }
-    // 건축물대장 발급 api
-    @Override
-    public DealResponse.Download getBuildingRegisterPdf(Long dealId) throws Exception {
-        // 1) DB 조회
-        DealDocumentInfo deal = dealMapper.getDocumentInfo(dealId);
-        // request json 형식에 맞게 파싱
-        BuildingRegisterRequest request = BuildingRegisterRequest.from(deal);
-        // 1차·2차가 섞여 있을 수 있는 응답(rawResponse)
-        String rawResponse = codefTwoFactorService.generalBuildingLeader(request);
-        // dto로 파싱
-        BuildingRegisterResponse dto =
-                CodefConverter.parseDataToDto(rawResponse, BuildingRegisterResponse.class);
-        // PDF 바이트 추출
-        byte[] pdfBytes = PdfUtil.decodePdfBytes(dto.getResOriGinalData());
-        /* ncp 업로드*/
-        String key  = "building-register/" + dealId + ".pdf";
-        String url = binaryUploaderService.putPdfObject(BUCKET_NAME,key,pdfBytes);
+    return path;
+  }
 
-        return new DealResponse.Download(url);
-    }
+  // 등기부등본 발급 api
+  @Override
+  public DealResponse.Download getEstateRegisternPdf(Long dealId)
+      throws Exception {
+    //DB에서 데이터 가져와서 request 생성
+    EstateRegistrationRequest request = dealMapper.getEstateRegistrationRequest(dealId);
+    // codef에서 응답 가져오기
+    String rawResponse = codefService.realEstateRegistrationLeader(request);
+    // pdf dto 파싱로직
+    EstateRegistrationResponse dto = CodefConverter.parseDataToDto(
+        rawResponse, EstateRegistrationResponse.class);
+    // PDF 바이트 추출
+    byte[] pdfBytes = PdfUtil.decodePdfBytes(dto.getResOriGinalData());
+    /* 6) ncp 업로드 */
+    String key = "estate-Register/" + dealId + ".pdf";
+    String url = binaryUploaderService.putPdfObject(BUCKET_NAME, key, pdfBytes);
+
+    return new DealResponse.Download(url);
+  }
+
+  // 건축물대장 발급 api
+  @Override
+  public DealResponse.Download getBuildingRegisterPdf(Long dealId) throws Exception {
+    // 1) DB 조회
+    DealDocumentInfo deal = dealMapper.getDocumentInfo(dealId);
+    // request json 형식에 맞게 파싱
+    BuildingRegisterRequest request = BuildingRegisterRequest.from(deal);
+    // 1차·2차가 섞여 있을 수 있는 응답(rawResponse)
+    String rawResponse = codefTwoFactorService.generalBuildingLeader(request);
+    // dto로 파싱
+    BuildingRegisterResponse dto =
+        CodefConverter.parseDataToDto(rawResponse, BuildingRegisterResponse.class);
+    // PDF 바이트 추출
+    byte[] pdfBytes = PdfUtil.decodePdfBytes(dto.getResOriGinalData());
+    /* ncp 업로드*/
+    String key = "building-register/" + dealId + ".pdf";
+    String url = binaryUploaderService.putPdfObject(BUCKET_NAME, key, pdfBytes);
+
+    return new DealResponse.Download(url);
+  }
 }
--- src/main/java/bjs/zangbu/security/util/JwtProcessor.java
+++ src/main/java/bjs/zangbu/security/util/JwtProcessor.java
@@ -4,85 +4,84 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Component
 public class JwtProcessor {
 
-    @Value("${jwt.secret}")
-    private String secretKeyPlain;
+  @Value("${jwt.secret}")
+  private String secretKeyPlain;
 
-    private final long accessTokenValidMs = 1000L * 60 * 2;       // 2분
-    private final long refreshTokenValidMs = 1000L * 60 * 60 * 24 * 14; // 14일
+  private final long accessTokenValidMs = 1000L * 60 * 2 * 100;       // 2분
+  private final long refreshTokenValidMs = 1000L * 60 * 60 * 24 * 14; // 14일
 
-    private Key key;
+  private Key key;
 
-    @PostConstruct
-    public void init() {
-        this.key = Keys.hmacShaKeyFor(secretKeyPlain.getBytes(StandardCharsets.UTF_8));
+  @PostConstruct
+  public void init() {
+    this.key = Keys.hmacShaKeyFor(secretKeyPlain.getBytes(StandardCharsets.UTF_8));
+  }
+
+  // Access Token 생성
+  public String generateAccessToken(String email, String role) {
+    return Jwts.builder()
+        .setSubject(email)
+        .claim("role", role)
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidMs))
+        .signWith(key)
+        .compact();
+  }
+
+  // Refresh Token 생성
+  public String generateRefreshToken(String email) {
+    return Jwts.builder()
+        .setSubject(email)
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + refreshTokenValidMs))
+        .signWith(key)
+        .compact();
+  }
+
+  //refresh 토큰 유효시간 반환
+  public long getRefreshTokenExpiration() {
+    return refreshTokenValidMs;
+  }
+
+  // username = email 추출
+  public String getEmail(String token) {
+    if (!validateToken(token)) {
+      throw new JwtException("유효하지 않은 토큰입니다.");
     }
+    return getClaims(token).getSubject();
+  }
 
-    // Access Token 생성
-    public String generateAccessToken(String email, String role) {
-        return Jwts.builder()
-                .setSubject(email)
-                .claim("role", role)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidMs))
-                .signWith(key)
-                .compact();
-    }
+  // role 추출
+  public String getRole(String token) {
+    return getClaims(token).get("role", String.class);
+  }
 
-    // Refresh Token 생성
-    public String generateRefreshToken(String email) {
-        return Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenValidMs))
-                .signWith(key)
-                .compact();
+  // 토큰 유효성 검사
+  public boolean validateToken(String token) {
+    try {
+      getClaims(token); // 유효하면 예외 발생 안 함
+      return true;
+    } catch (JwtException | IllegalArgumentException e) {
+      return false;
     }
+  }
 
-    //refresh 토큰 유효시간 반환
-    public long getRefreshTokenExpiration(){
-        return refreshTokenValidMs;
-    }
-
-    // username = email 추출
-    public String getEmail(String token) {
-        if (!validateToken(token)) {
-            throw new JwtException("유효하지 않은 토큰입니다.");
-        }
-        return getClaims(token).getSubject();
-    }
-
-    // role 추출
-    public String getRole(String token) {
-        return getClaims(token).get("role", String.class);
-    }
-
-    // 토큰 유효성 검사
-    public boolean validateToken(String token) {
-        try {
-            getClaims(token); // 유효하면 예외 발생 안 함
-            return true;
-        } catch (JwtException | IllegalArgumentException e) {
-            return false;
-        }
-    }
-
-    // Claims 추출 (토큰 파싱)
-    private Claims getClaims(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key) //jwt 생성/검증 시 사용
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
+  // Claims 추출 (토큰 파싱)
+  private Claims getClaims(String token) {
+    return Jwts.parserBuilder()
+        .setSigningKey(key) //jwt 생성/검증 시 사용
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+  }
 }
