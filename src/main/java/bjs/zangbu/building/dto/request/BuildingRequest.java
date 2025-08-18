400039bca990271d3808ee5f70b99baaacd32560
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-08-18 14:47:30 +0900

    1
    1

--- src/main/java/bjs/zangbu/building/dto/request/BuildingRequest.java
+++ src/main/java/bjs/zangbu/building/dto/request/BuildingRequest.java
@@ -333,7 +333,7 @@ public class BuildingRequest {
         private PropertyType propertyType;
         // 입주 가능 날짜
         private LocalDateTime moveDate;
-        // 한 줄 소개 문구
+        // 한 줄 소개 문구a
         private String infoOneline;
         // 건물 상세 설명
         private String infoBuilding;
@@ -343,6 +343,8 @@ public class BuildingRequest {
         private String contactPhone;
         // 제공되는 시설 정보 (예: 엘리베이터, 주차장, 보안 등)
         private String facility;
+        // 집 면적
+        private float size;
     }
 }
 
